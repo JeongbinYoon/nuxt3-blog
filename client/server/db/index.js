be93aa3e425db5d56b8066c621e6f62c7f61e50a
JeongbinYoon <yon980824@gmail.com>
2024-01-11 00:35:12 +0900

    MySQL 파일 분리, 연결 풀 생성

--- client/server/db/index.js
+++ client/server/db/index.js
@@ -1,12 +1,18 @@
-import mysql from 'mysql2';
+import mysql from "mysql2/promise";
 
-const connection = mysql.createConnection({
+// MySQL 연결 풀 생성
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASWWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-// connection.connect();
 
-export default connection;
+// 풀에서 연결 가져오기
+const getMySQLConnection = () => pool.getConnection();
+
+export default getMySQLConnection;
