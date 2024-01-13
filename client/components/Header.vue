bc1eec4bd214c783776116e3fa478df962f82a82
JeongbinYoon <yon980824@gmail.com>
2024-01-14 01:35:35 +0900

    header 작업, scss 적용

---
+++ client/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="header">
+    <div class="max-container flex justify-sb align-c">
+      <h1 class="logo">blog</h1>
+      <p class="title">타틀타이틀이타이틀타</p>
+      <div>
+        <button>search</button>
+        <button>theme</button>
+      </div>
+    </div>
+  </div>
+</template>
