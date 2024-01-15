86bb902d299dc7f55586682b512463eb8767b944
JeongbinYoon <yon980824@gmail.com>
2024-01-15 23:43:00 +0900

    CKEditor 연동

---
+++ client/pages/write.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ClientOnly>
+      <Editor v-model="content" />
+    </ClientOnly>
+    <div>content is:{{ content }}</div>
+  </div>
+</template>
+<script setup>
+import Editor from '~/components/editor';
+const content = ref();
+</script>
