f33ac35b583a25848dc6faf06282032689c12e62
JeongbinYoon <yon980824@gmail.com>
2024-01-19 00:44:51 +0900

    Noto Sans 적용, 아이콘 fontawesome 적용

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,14 +1,28 @@
+<script setup>
+const theme = ref(true);
+const changeTheme = () => (theme.value = !theme.value);
+</script>
+
 <template>
   <div id="header">
     <div class="max-container flex justify-sb align-c">
-      <h1 class="logo">blog</h1>
+      <h1 class="logo">
+        <NuxtLink to="/">blog</NuxtLink>
+      </h1>
       <p class="title">타틀타이틀이타이틀타</p>
-      <div>
-        <button>search</button>
-        <button>theme</button>
+      <div class="header-btns">
         <button>
-          <NuxtLink to="/write"> 새 글 작성 </NuxtLink>
+          <font-awesome-icon icon="magnifying-glass" />
         </button>
+
+        <button v-if="theme" @click="theme = !theme">
+          <font-awesome-icon icon="moon" />
+        </button>
+        <button v-else @click="theme = !theme">
+          <font-awesome-icon icon="lightbulb" />
+        </button>
+
+        <NuxtLink class="write-btn" to="/write"> 새 글 작성 </NuxtLink>
       </div>
     </div>
   </div>
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/index.scss', '~/assets/css/common.scss'],
+  css: [
+    '~/assets/css/index.scss',
+    '~/assets/css/common.scss',
+    '@fortawesome/fontawesome-svg-core/styles.css',
+  ],
 });
