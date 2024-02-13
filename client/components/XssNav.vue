c088949623d3dfd3847f18bf1242bc3d9a0a114f
JeongbinYoon <yon980824@gmail.com>
2024-02-13 23:18:20 +0900

    xss 실습 페이지 생성

---
+++ client/components/XssNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/learn/xss/reflected">Reflected XSS</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/learn/xss/stored">Stored XSS</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/learn/xss/dom-based">Dom-based XSS</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ client/pages/learn/xss/dom-based.vue
@@ -0,0 +1,41 @@
+<script setup>
+definePageMeta({
+  layout: 'empty',
+});
+
+const title = ref('');
+const contents = ref('');
+
+const submit = () => {
+  console.log('title', title.value);
+  console.log('contents', contents.value);
+};
+</script>
+
+<template>
+  <div id="stored-xss">
+    <h1>Dom-based XSS</h1>
+    <XssNav />
+
+    <div>
+      <ul>
+        <li>asdas</li>
+        <li>asdas</li>
+      </ul>
+      <div>
+        <label for="title">제목</label>
+        <input v-bind="title" type="text" id="title" />
+
+        <label for="contents">내용</label>
+        <textarea
+          v-bind="contents"
+          id="contents"
+          cols="30"
+          rows="10"
+        ></textarea>
+
+        <button @click="submit">저장</button>
+      </div>
+    </div>
+  </div>
+</template>
