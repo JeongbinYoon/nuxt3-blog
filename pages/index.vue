2e62cb3277d89556af9ae5e5eca0756b000b202b
JeongbinYoon <yon980824@gmail.com>
2024-02-29 22:56:19 +0900

    메인 페이지 임시 리다이렉트

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
+<script setup>
+const router = useRouter();
+router.replace('/category/all');
+</script>
 <template>
   <div id="main" class="content-padding">
-    <p>main</p>
+    <!-- <p>main</p> -->
   </div>
 </template>
